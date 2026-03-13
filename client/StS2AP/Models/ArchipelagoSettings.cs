5f4506a96005228021051a6c4f1c767de83aa8c6
Douglas Lueben <dlueben1@umbc.edu>
2026-03-12 21:11:53 -0400

    feat: read relevant archipelago player settings
    The same settings they set in their YAML when configuring the game

---
+++ client/StS2AP/Models/ArchipelagoSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StS2AP.Models
+{
+    /// <summary>
+    /// The settings that a player has configured for their Archipelago Slot
+    /// </summary>
+    public class ArchipelagoSettings
+    {
+        public int AscensionLevel { get; set; }
+
+        /// <summary>
+        /// Whether all cards should be shuffled or not - if not, only every other card will be an AP Item
+        /// </summary>
+        public bool ShouldShuffleAllCards { get; set; }
+
+        public bool IsSeeded { get; set; }
+    }
+}
--- client/StS2AP/Utils/GameUtility.cs
+++ client/StS2AP/Utils/GameUtility.cs
@@ -1,10 +1,13 @@
-﻿using MegaCrit.Sts2.Core.Entities.Players;
+﻿using Archipelago.MultiClient.Net.Models;
+using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Characters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static StS2AP.Data.CharTable;
 
 namespace StS2AP.Utils
 {
@@ -29,6 +32,46 @@ namespace StS2AP.Utils
         /// `GameUtility.UnlockedCharacters.Add(ModelDb.Character<Characters.Necrobinder>());`
         /// </summary>
         public static List<CharacterModel> UnlockedCharacters { get; set; } = new List<CharacterModel>();
+
+        #endregion
+
+        #region Processing Incoming Items
+
+        /// <summary>
+        /// Unlocks a Character for the player.
+        /// </summary>
+        public static void UnlockCharacter(ItemInfo item)
+        {
+            CharacterModel? characterToUnlock = null;
+            switch(item.GetStSCharID())
+            {
+                case APItemCharID.Ironclad:
+                    characterToUnlock = ModelDb.Character<Ironclad>();
+                    break;
+                case APItemCharID.Silent:
+                    characterToUnlock = ModelDb.Character<Silent>();
+                    break;
+                case APItemCharID.Defect:
+                    characterToUnlock = ModelDb.Character<Defect>();
+                    break;
+                case APItemCharID.Regent:
+                    characterToUnlock = ModelDb.Character<Regent>();
+                    break;
+                case APItemCharID.Necrobinder:
+                    characterToUnlock = ModelDb.Character<Necrobinder>();
+                    break;
+            }
+
+            // If we didn't find the character there's a problem to log
+            if(characterToUnlock == null)
+            {
+                LogUtility.Warn($"Could not find character to unlock for item {item.ItemName} (Char ID Parsed: {item.GetStSCharID().ToString()})");
+                return;
+            }
+
+            // Add the character to the game
+            if(!UnlockedCharacters.Contains(characterToUnlock)) UnlockedCharacters.Add(characterToUnlock);
+        }
 
         #endregion
     }
