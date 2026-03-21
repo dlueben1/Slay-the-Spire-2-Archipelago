cfa5003c1ed83bfcd5abbf836604ef75590feefb
platanobailando <cjmang@gmail.com>
2026-03-20 23:38:50 -0500

    Fixing generation issue

--- world/spire2/test/option_tests.py
+++ world/spire2/test/option_tests.py
@@ -13,4 +13,24 @@ class TestMultiCharsValid(Spire2TestBase):
 
     def test_valid(self):
         CharacterOptions.schema.validate(self.world.options.advanced_characters.value)
-        pass
+
+class Test49Floors(Spire2TestBase):
+    options = {
+        "characters": [
+            "ironclad",
+            "silent",
+        ],
+        "ascension": 10
+    }
+
+    def test_ensure_floor_49(self):
+        self.assertIsNotNone(self.world.get_location("Ironclad Reached Floor 49"))
+
+    def test_no_dupes(self):
+        stuff = dict()
+
+        for location in self.world.get_locations():
+            if location.address is None:
+                continue
+            self.assertTrue(location.address not in stuff, f"location duplicated {location.name} {stuff.get(location.address, None)}")
+            stuff[location.address] = location
