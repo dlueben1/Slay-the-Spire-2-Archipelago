5d4abc5c4efed272f42c1077357b24b40077df37
Douglas Lueben <dlueben1@umbc.edu>
2026-08-25 23:06:38 -0400

    feat: add apworld support for setting and generating bonus items

--- world/spire2/web_world.py
+++ world/spire2/web_world.py
@@ -7,7 +7,7 @@ from .options import (
     IncludeFloorChecks, NeowSanity, CampfireSanity, GoldSanity, PotionSanity,
     ShopSanity, ShopCardSlots, ShopNeutralSlots, ShopRelicSlots, ShopPotionSlots, ShopRemoveSlots, ShopSanityCosts,
     AncientRelicLocation, AncientRelicPool, RelicRewardsAvailableAnytime, ReleaseOnVictory,
-    CardReward, ProgressiveStarterCard, ProgressiveStarterRelic, SeededRun,
+    CardReward, ProgressiveStarterCard, ProgressiveStarterRelic, BonusItems, SeededRun,
     OneGoldFillerWeight, FiveGoldFillerWeight,
     FreeAttackFillerWeight, FreePowerFillerWeight, FreeSkillFillerWeight,
     DexterityFillerWeight, StrengthFillerWeight, PlatingFillerWeight,
@@ -49,7 +49,7 @@ class SlayTheSpire2Web(WebWorld):
             ProgressiveStarterCard,
             ProgressiveStarterRelic,
             CardReward,
-        ]),
+        ]),        
         OptionGroup("Sanities", [
             IncludeFloorChecks,
             NeowSanity,
@@ -75,6 +75,9 @@ class SlayTheSpire2Web(WebWorld):
             AdvancedChar,
             CharacterOptions,
         ], start_collapsed=True),
+        OptionGroup("Bonus Items", [
+            BonusItems,
+        ]),
         OptionGroup("Filler Items", [
             OneGoldFillerWeight,
             FiveGoldFillerWeight,
